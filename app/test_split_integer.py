e21512e209c49fb32fafca95540bb664b8f26a13
Mykyta Tkachenko <11Draghost11@gmail.com>
2026-06-16 21:06:39 +0300

    fixes

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -6,7 +6,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    assert split_integer(12, 4) == [3,3,3,3]
+    assert split_integer(12, 4) == [3, 3, 3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
@@ -20,4 +20,4 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    assert split_integer(3, 5) == [0,0,1,1,1]
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
