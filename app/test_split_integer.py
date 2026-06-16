a07e167c3142eccd79cf68ad221b5a37d0f7d51c
Mykyta Tkachenko <11Draghost11@gmail.com>
2026-06-16 21:13:31 +0300

    fixes

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -20,5 +20,6 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     assert split_integer(3, 5) == [0, 0, 1, 1, 1]
 
+
 def test_difference_between_max_and_min_should_not_exceed_one() -> None:
     assert max(split_integer(17, 4)) - min(split_integer(17, 4)) <= 1
